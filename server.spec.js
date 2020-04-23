55c7a6f361bbfcc29346dc392961f6d78094f87a
LeRoyce Pearson <leroycepearson@geemili.xyz>
2020-04-23 14:54:59 -0600

    Remove PUT test case todos
    I've decided not to make the PUT route

--- server.spec.js
+++ server.spec.js
@@ -85,15 +85,6 @@ describe("server", () => {
     });
   });
 
-  describe("PUT /api/blocks/:id", () => {
-    it.todo("should return 400 error if a required field is missing");
-    it.todo("should return 400 error if an invalid field is included");
-    it.todo("should return 404 error if specified block does not exist");
-    it.todo("should return a status code of 201");
-    it.todo("should return the updated block");
-    it.todo("should reject duplicate block names");
-  });
-
   describe("DELETE /api/blocks/:id", () => {
     const diamondBlock = {
       id: 10,
