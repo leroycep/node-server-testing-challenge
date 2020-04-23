17fbc1745f1f8eaf707c234e9cea8d0d8e310063
LeRoyce Pearson <leroycepearson@geemili.xyz>
2020-04-23 13:57:42 -0600

    Write test cases for GET /api/blocks

--- server.js
+++ server.js
@@ -4,4 +4,10 @@ const server = express();
 
 server.use(express.json());
 
+let blocks = [];
+
+server.get("/api/blocks", (req, res) => {
+  res.status(200).json(blocks);
+});
+
 module.exports = server;
--- server.spec.js
+++ server.spec.js
@@ -1,13 +1,23 @@
+const request = require("supertest");
+const server = require("./server.js");
+
 describe("server", () => {
   describe("GET /api/blocks", () => {
-    it.todo("should return a status code of 200");
-    it.todo("should return a list of blocks");
+    it("should return a status code of 200", async () => {
+      const response = await request(server).get("/api/blocks");
+      expect(response.status).toEqual(200);
+    });
+    it("should return a list of blocks", async () => {
+      const response = await request(server).get("/api/blocks");
+      expect(response.body).toBeInstanceOf(Array);
+    });
   });
   describe("POST /api/blocks", () => {
     it.todo("should return 400 error if a required field is missing");
     it.todo("should return 400 error if an invalid field is included");
     it.todo("should return a status code of 201");
     it.todo("should return the newly created block");
+    it.todo("should add the block to the list of all blocks");
     it.todo("should reject duplicate block names");
   });
   describe("PUT /api/blocks/:id", () => {
